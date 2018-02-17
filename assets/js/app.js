27839a774eaed204b93f1cc4dafb36511e7bd338
SiriusScaper <siriusscaper@gmail.com>
2018-02-16 22:23:16 -0800

    Added variables, start of functions, HTML structure

---
+++ assets/js/app.js
@@ -0,0 +1,22 @@
+//Select elements and store in variable
+const quizContainer = document.getElementById('quiz')
+const resultContainer = document.getElementById('result')
+const buttonA = document.getElementById('choiceA')
+const buttonB = document.getElementById('choiceB')
+const buttonC = document.getElementById('choiceC')
+
+//Function to startQuiz
+let startQuiz = () => {
+
+}
+
+//Function to show result
+
+let checkAnswer = () => {
+
+}
+
+
+//Need functions to display questions and answers to DOM
+    //Probably the jquery "because it's easier" 
+
